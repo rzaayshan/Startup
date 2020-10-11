1978d2f17d31910f122385c5ba5cff51effdf47c
ElbrusGarayev <60807221+ElbrusGarayev@users.noreply.github.com>
2020-10-11 13:50:24 +0400

    Category entity changed

--- src/main/java/app/entity/Category.java
+++ src/main/java/app/entity/Category.java
@@ -1,11 +1,16 @@
 package app.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Set;
 
 @Data
@@ -13,18 +18,24 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "c_id")
     private long id;
+
     private String name;
 
     @OneToMany(mappedBy = "category")
-    Set<Product> products;
+    private Set<Product> products;
 
     @OneToMany(mappedBy = "category")
     Set<CategoryFeature> categoryFeatures;
 
-    @OneToOne
-    private ParentCategory parentCategory;
+    @ManyToOne(cascade={CascadeType.ALL})
+    @JoinColumn(name = "parent_category_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> subCategory;
 }
